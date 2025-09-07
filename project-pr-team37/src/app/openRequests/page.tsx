71d11fc7aaed4abc1ecc9d821b084ac9d6de5eea
Trevor-04 <trevortopolski@icloud.com>
2025-09-07 16:06:30 -0500

    added filter and open/closed pages

--- project-pr-team37/src/app/openRequests/page.tsx
+++ project-pr-team37/src/app/openRequests/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Filter from '../components/filter';
 
 export default function openRequests() {
   return (
     <div className='bg-[#0d1117] h-screen'>
-      Open requests
+      <Filter />
     </div>
   )
 }
