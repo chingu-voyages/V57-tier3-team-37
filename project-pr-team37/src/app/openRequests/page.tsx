f9acbb5113ea3666b33d243e1368d630850011d1
Spandan Mahat <spandanmahat00@gmail.com>
2025-09-08 13:04:54 -0500

    starting to code on open PRs page

--- project-pr-team37/src/app/openRequests/page.tsx
+++ project-pr-team37/src/app/openRequests/page.tsx
@@ -5,6 +5,7 @@ export default function openRequests() {
   return (
     <div className='bg-[#0d1117] h-screen'>
       <Filter />
+      <h1 className="font-bold">Open Pull Requests</h1>
     </div>
   )
 }
